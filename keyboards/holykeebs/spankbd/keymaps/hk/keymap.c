4c007a1cc1b059f49754c4e0e0bd3f8fb204747e
Idan Kamara <i@idank.me>
2026-03-27 13:25:06 +0300

    fix: replace deprecated RGB and mouse keycodes removed in qmk#25444
    Replace RGB_TOG/RGB_MOD/etc with RM_* (RGB Matrix) or UG_* (RGBLIGHT)
    and KC_BTN*/KC_MS_* with MS_* across crkbd, keyball, spankbd, and sweeq
    keymaps.

--- keyboards/holykeebs/spankbd/keymaps/hk/keymap.c
+++ keyboards/holykeebs/spankbd/keymaps/hk/keymap.c
@@ -79,7 +79,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_NUMBERS] = LAYOUT_split_3x5_3(
          KC_1,  KC_2,  KC_3,  KC_4,  KC_5,           KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
          KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_VOLU,
-         KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,         KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_VOLD,
+         KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,         MS_LEFT, MS_DOWN, MS_UP,   MS_RGHT, KC_VOLD,
                     KC_F11, KC_F12, KC_TRNS,         KC_TRNS,  KC_MPLY,  KC_MNXT),
 
     [_POINTER] = LAYOUT_split_3x5_3(
